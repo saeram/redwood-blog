054cd26f09d90546b4cf81f5019a028fce5bfbc8
saeram <seram1427@gmail.com>
2024-06-14 11:42:24 +0900

    redwood project set up

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+    Home
+    </>
+  )
+}
+
+export default HomePage
